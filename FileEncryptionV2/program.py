081e6582b96ecbf43cfdefa6f86f5b1751229188
fredrikj31 <fredrik1206@gmail.com>
2019-10-25 07:57:35 +0200

    Updated the to cryption methods. Removed the debug

--- FileEncryptionV2/program.py
+++ FileEncryptionV2/program.py
@@ -1,23 +1,36 @@
+import base64
+import os
 from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-def getKey():
-	file = open('key.key', 'rb')
-	key = file.read()
-	file.close()
+def getKey(password):
+	password_provided = password # This is input in the form of a string
+	password = password_provided.encode() # Convert to type bytes
+	salt = b'salt_' # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
+	kdf = PBKDF2HMAC(
+		algorithm=hashes.SHA256(),
+		length=32,
+		salt=salt,
+		iterations=100000,
+		backend=default_backend()
+	)
+	key = base64.urlsafe_b64encode(kdf.derive(password))
 
-	print(key)
-	
 	f = Fernet(key)
+
 	return f
 
 def encryptFile():
 	OpenFile = input("What file should be encrypted? >> ")
+	CryptPassword = input("Enter the password to encrypt this file >> ")
 
 	try:
 		file = open(OpenFile, 'r')
 		TextFile = file.read()
 
-		encryptedText = getKey().encrypt(TextFile.encode())
+		encryptedText = getKey(CryptPassword).encrypt(TextFile.encode())
 
 		fileNameExtension = OpenFile.split('.', 1)[0]
 		newFileName = fileNameExtension + "-Encrypted" + ".txt"
@@ -29,15 +42,21 @@ def encryptFile():
 
 	except IOError:
 		print("File not found!")
+		exit()
 
 def decryptFile():
 	OpenFile = input("What file should be decrypted? >> ")
+	CryptPassword = input("Enter the password to encrypt this file >> ")
 
 	try:
 		file = open(OpenFile, 'r')
 		TextFile = file.read()
 
-		decryptedText = getKey().decrypt(TextFile.encode())
+		try:
+			decryptedText = getKey(CryptPassword).decrypt(TextFile.encode())
+		except:
+			print("Wrong password to unlock file.")
+			exit()
 
 		fileNameExtension = OpenFile.split('-Encrypted', 1)[0]
 		newFileName = fileNameExtension + "-Decrypted" + ".txt"
@@ -49,6 +68,7 @@ def decryptFile():
 
 	except IOError:
 		print("File not found!")
+		exit()
 
 if __name__ == "__main__":
 	print("Welcome to file encryption!")
